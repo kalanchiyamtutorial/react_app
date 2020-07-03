3615c63a77d849d8a6ef736d5835995ef6eadc6c
kalanchiyamtutorial@gmail.com <kalanchiyamtutorial@gmail.com>
2020-07-03 14:02:34 +0530

    Updates

---
+++ precache-manifest.fde8b13f0d0eb3b93d31a49c8513594a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a01d67f3040cdcb6008db2dee9dfd74",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "0cd026caf5ca67e5eb1d",
+    "url": "/react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "db24f21c29e31f7611be",
+    "url": "/react-app/static/js/2.d185b933.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.d185b933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cd026caf5ca67e5eb1d",
+    "url": "/react-app/static/js/main.b404e036.chunk.js"
+  },
+  {
+    "revision": "579e4e0adc9f8620e4f2",
+    "url": "/react-app/static/js/runtime-main.2d87a4b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
